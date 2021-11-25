aa3831aad071954c1b1b6be7ff3219dc77bc0876
mojokb <amaramusic@gmail.com>
2021-11-25 08:02:01 +0000

    Update bentoservice

--- bento_pack/bento_service.py
+++ bento_pack/bento_service.py
@@ -1,10 +1,11 @@
 import bentoml
-
 from torchvision import transforms
 from net import Net
 from bentoml.adapters import ImageInput
 from bentoml.frameworks.pytorch import PytorchModelArtifact
 from prometheus_client import Summary
+import torch
+import torch.nn.functional as f
 
 REQUEST_TIME = Summary(name='predict_request_processing_time',
                        documentation='Time spend predict processing request',
@@ -26,6 +27,7 @@ class PytorchModelService(bentoml.BentoService):
     def predict(self, img):
         x = self.transform(img)
         outputs = self.artifacts.model(x)
-        probs, output_classes = outputs.max(dim=1)
+        _, output_classes = outputs.max(dim=1)
+        probs = torch.max(f.softmax(outputs))
         return {"probs": "{:.1%}".format(probs.item()),
                 "output_classes": output_classes.item()}
