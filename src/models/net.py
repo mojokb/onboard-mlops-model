dca500ed224d8dba51ea4a5a6bea25c85d14ca5e
mojokb <amaramusic@gmail.com>
2021-12-01 07:00:31 +0000

    new train

--- src/models/net.py
+++ src/models/net.py
@@ -6,8 +6,8 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(784, 64)
-        self.fc2 = nn.Linear(64, 10)
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = x.float()
